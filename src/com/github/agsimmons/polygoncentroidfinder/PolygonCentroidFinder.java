14f01df434367690b45b41d5444903b58fad1c30
Andrew Simmons <agsimmons0@gmail.com>
2017-04-23 20:11:47 -0400

    Initial Commit

---
+++ src/com/github/agsimmons/polygoncentroidfinder/PolygonCentroidFinder.java
@@ -0,0 +1,17 @@
+package com.github.agsimmons.polygoncentroidfinder;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PolygonCentroidFinder extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
